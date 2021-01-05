cffd97d2482e73993ed0d33d6c1d3ff3d1f69795
Abdul Basit <basit26374@gmail.com>
2021-01-06 00:04:04 +0500

    Passing Recipe data with property Binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,9 @@ import { Recipe } from '../recipe.model'
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Test Recipe', 'Test recipe Description',
+    'https://www.forksoverknives.com/wp-content/uploads/vegan-quesadilla-recipe-quick-easy-plant-based-recipes.jpg'),
+    
+    new Recipe('Test 2 Recipe', 'Test recipe Description',
     'https://www.forksoverknives.com/wp-content/uploads/vegan-quesadilla-recipe-quick-easy-plant-based-recipes.jpg')
   ]
 
