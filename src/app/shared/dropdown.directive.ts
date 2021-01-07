f77593b4013c01bbf45b979df2049274a59a6dae
Abdul Basit <basit26374@gmail.com>
2021-01-07 14:24:10 +0500

    On previous commit, dropdown is just closed when user click again on that button. In this commit, dropdown is closed even user will click outside the button area.

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,13 +1,14 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, HostBinding, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
-export class DropdownDirective {
+export class DropdownDirective   {
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
-  }
-
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+  this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  console.log(event)
+}
+constructor(private elRef: ElementRef) {}
 }
